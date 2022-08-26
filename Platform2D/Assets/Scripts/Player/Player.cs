1193653d7578cb1a9d9c7910bc5e7839c80fa198
CarolUrbs <caroline.urbainski@edu.univali.br>
2022-08-26 12:56:15 -0300

    Movimentação
    criada a movimentação do personagem em uma cena de protótipo

---
+++ Platform2D/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public Vector2 velocity;
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //myRigidbody.MovePosition(myRigidbody.position-velocity*Time.deltaTime);
+            myRigidbody.velocity = new Vector2(-speed,myRigidbody.velocity.y);
+
+        }
+      else   if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
+            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+
+        }
+    }
+}
