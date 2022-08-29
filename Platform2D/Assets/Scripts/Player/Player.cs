700cad09a6d25e91f363a911454f4c99f23aec96
CarolUrbs <caroline.urbainski@edu.univali.br>
2022-08-29 14:24:18 -0300

    camera e animação
    Implementada câmera e animação

--- Platform2D/Assets/Scripts/Player/Player.cs
+++ Platform2D/Assets/Scripts/Player/Player.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    [Header("Speed Setup")]
     public Vector2 friction = new Vector2 (-.1f,0);
     public float speed;
     public float speedRun;
     private float _currentSpeed;
-    public float forceJump = 2; 
+    public float forceJump = 2;
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = -.5f;
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
 
     void Update()
     {
@@ -56,7 +63,28 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody.velocity = Vector2.up * forceJump;
+            myRigidbody.transform.localScale = Vector2.one;
 
+            DOTween.Kill(myRigidbody.transform);
+
+            HandleScaleJump();
+        }
+    }
+    private void HandleScaleJump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY,animationDuration).SetLoops(2,LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag =="floor")
+        {
+            myRigidbody.transform.DOScaleY(-jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+            myRigidbody.transform.DOScaleX(-jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+            myRigidbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidbody.transform);
         }
     }
 }
