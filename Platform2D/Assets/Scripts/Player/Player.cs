034e396fef7599ad0d1ac14723122df68842c957
CarolUrbs <caroline.urbainski@edu.univali.br>
2022-08-29 12:09:04 -0300

    pulo
    implementação do pulo do personagem

--- Platform2D/Assets/Scripts/Player/Player.cs
+++ Platform2D/Assets/Scripts/Player/Player.cs
@@ -5,21 +5,45 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2 (-.1f,0);
     public float speed;
+    public float forceJump = 2; 
 
     void Update()
+    {
+        VerticalMoviment();
+        HorizontalMoviment();
+    }
+
+    void  HorizontalMoviment()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position-velocity*Time.deltaTime);
-            myRigidbody.velocity = new Vector2(-speed,myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
 
         }
-      else   if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+
+        }
+        if (myRigidbody.velocity.x>0)
+        {
+            myRigidbody.velocity += friction;
+        }
+        else if (myRigidbody.velocity.x<0)
+        {
+            myRigidbody.velocity -= friction;
+
+        }
+    }
+    void VerticalMoviment()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * forceJump;
 
         }
     }
