be28e7731f7fad799a560ef55add58f657165886
CarolUrbs <caroline.urbainski@edu.univali.br>
2022-11-07 19:22:13 -0300

    Coins!

---
+++ Platform2D/Assets/Scripts/Collectables/ItemCollectableBase.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollectableBase: MonoBehaviour
+{
+    public string compareTag = "Player";
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.CompareTag(compareTag))
+        {
+            Collect();
+        }
+    }
+    protected virtual void Collect()
+    {
+        Debug.Log("Collect");
+        gameObject.SetActive(false);
+        OnCollect();
+    }
+    protected virtual void OnCollect()
+    {
+
+    }
+}
