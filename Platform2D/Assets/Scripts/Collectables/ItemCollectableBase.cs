a9d4ab119c015194571adeb92435becfa23d24e5
CarolUrbs <caroline.urbainski@edu.univali.br>
2022-12-05 21:31:32 -0300

    VFX
    Efeitos da moeda e da caminhada do personagem

--- Platform2D/Assets/Scripts/Collectables/ItemCollectableBase.cs
+++ Platform2D/Assets/Scripts/Collectables/ItemCollectableBase.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class ItemCollectableBase: MonoBehaviour
 {
     public string compareTag = "Player";
+    public ParticleSystem _particleSystem;
+    private void Awake()
+    {
+        if (_particleSystem != null)   _particleSystem.transform.SetParent(null);
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.transform.CompareTag(compareTag))
@@ -18,5 +25,11 @@ public class ItemCollectableBase: MonoBehaviour
         gameObject.SetActive(false);
         OnCollect();
     }
-    protected virtual void OnCollect(){}
+    protected virtual void OnCollect()
+    {
+    if (_particleSystem != null)
+        {
+            _particleSystem.Play();
+        }
+    }
 }
